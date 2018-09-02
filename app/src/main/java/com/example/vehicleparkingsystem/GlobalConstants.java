fded3b60ba3e30bdcc009ec742597525981a10d1
KS Koh <kahsiang1123@outlook.com>
2018-09-02 22:32:16 +0800

    refactor(login): beautify code
    implement object-oriented concept
    provide useful feedback

---
+++ app/src/main/java/com/example/vehicleparkingsystem/GlobalConstants.java
@@ -0,0 +1,6 @@
+package com.example.vehicleparkingsystem;
+
+public class GlobalConstants {
+    public static final int CODE_GET_REQUEST = 1024;
+    public static final int CODE_POST_REQUEST = 1025;
+}
--- app/src/main/java/com/example/vehicleparkingsystem/LoginActivity.java
+++ app/src/main/java/com/example/vehicleparkingsystem/LoginActivity.java
@@ -1,39 +1,93 @@
 package com.example.vehicleparkingsystem;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import org.apache.http.NameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class LoginActivity extends AppCompatActivity{
+import java.util.HashMap;
 
-    private Button loginButton;
+public class LoginActivity extends AppCompatActivity implements OnTaskCompleted{
     private EditText emailEdit, passwordEdit;
     private TextView errorText;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Button loginButton;
+
         loginButton = findViewById(R.id.buttonLogin);
         emailEdit = findViewById(R.id.editEmail);
         passwordEdit = findViewById(R.id.editPassword);
         errorText  = findViewById(R.id.textError);
+        progressBar = findViewById(R.id.progressBar);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                verifyLogin();
+            }
+        });
     }
 
-    public void loginPost(View view){
-        String email = emailEdit.getText().toString();
-        String password = passwordEdit.getText().toString();
+    private void verifyLogin(){
+        String email = emailEdit.getText().toString().trim();
+        String password = passwordEdit.getText().toString().trim();
 
+        HashMap<String, String> params = new HashMap<>();
+        params.put("email", email);
+        params.put("password", password);
 
-        new SigninActivity(this, errorText).execute(email,password);
-        Toast.makeText(this, "Loading...", Toast.LENGTH_SHORT).show();
+        //Calling the getUserPassword API
+        PerformNetworkRequest request = new PerformNetworkRequest(Api.URL_GET_USER_PASSWORD, params, GlobalConstants.CODE_POST_REQUEST, progressBar, this);
+        request.execute();
+    }
+
+    public void onTaskCompleted(JSONObject object){
+        String success = "";
+        String userExist = "";
+        String message = "";
+
+        try{
+            success = object.getString("success");
+            message = object.getString("message");
+            try{
+                userExist = object.getString("user_exist");
+            }
+            catch(JSONException e){
+                userExist = "0";
+            }
+        }
+        catch(JSONException e){
+            Log.e("log_tag", "Error parsing data " + e.toString());
+        }
+
+        if(success.equals("1")){
+            if(userExist.equals("1")){
+                Intent intent=new Intent(this,ParkingActivity.class);
+                this.startActivity(intent);
+                this.errorText.setText("");
+                this.errorText.setVisibility(View.GONE);
+            }
+            else{
+                this.errorText.setText(message);
+                this.errorText.setVisibility(View.VISIBLE);
+            }
+        }
+        else{
+            this.errorText.setText(message);
+            this.errorText.setVisibility(View.VISIBLE);
+        }
     }
 }
