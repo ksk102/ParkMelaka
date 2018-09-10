5f9f3f4f4bab93f75f9f1a031a95f4a462e88398
zorotan <t.j.wei98@gmail.com>
2018-09-10 23:51:39 +0800

    design Login interface

--- app/src/main/java/com/example/vehicleparkingsystem/Api.java
+++ app/src/main/java/com/example/vehicleparkingsystem/Api.java
@@ -1,6 +1,6 @@
 package com.example.vehicleparkingsystem;
 
 public class Api {
-    private static final String ROOT_URL = "http://192.168.1.8/ParkMelaka_WebServices/api.php?api=";
+    private static final String ROOT_URL = "http://parkmelaka-webservices.000webhostapp.com/api.php?api=";
     public static final String URL_GET_USER_PASSWORD = ROOT_URL + "getUserPassword";
 }
--- app/src/main/java/com/example/vehicleparkingsystem/LoginActivity.java
+++ app/src/main/java/com/example/vehicleparkingsystem/LoginActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -23,7 +25,17 @@ public class LoginActivity extends AppCompatActivity implements OnTaskCompleted{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //set content view AFTER ABOVE sequence (to avoid crash)
+        this.setContentView(R.layout.activity_login);
+
+
 
         Button loginButton;
 
