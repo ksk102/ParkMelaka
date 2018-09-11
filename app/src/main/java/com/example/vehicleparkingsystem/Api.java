2c144b446ab873b42929311111e39e30508d5db6
KS Koh <kahsiang1123@outlook.com>
2018-09-11 17:56:13 +0800

    refactor(file): clear unused files

--- app/src/main/java/com/example/vehicleparkingsystem/Api.java
+++ app/src/main/java/com/example/vehicleparkingsystem/Api.java
@@ -1,7 +1,7 @@
 package com.example.vehicleparkingsystem;
 
 public class Api {
-    private static final String ROOT_URL = "http://parkmelaka-webservices.000webhostapp.com/api.php?api=";
+    private static final String ROOT_URL = "http://192.168.1.9/ParkMelaka_WebServices/api.php?api=";
     public static final String URL_GET_USER_PASSWORD = ROOT_URL + "getUserPassword";
     public static final String URL_GET_USER_DETAIL = ROOT_URL + "getUserDetail";
 }
